04774fad2188d16fcea6405a81172ff733e878a0
Keluaa <34173752+Keluaa@users.noreply.github.com>
2023-07-21 09:11:13 +0200

    Optimized NVTX profiling. Ignore `NVTX.isactive()`

--- ext/ArmonNVTX.jl
+++ ext/ArmonNVTX.jl
@@ -5,14 +5,19 @@ isdefined(Base, :get_extension) ? (import NVTX) : (import ..NVTX)
 
 
 const armon_domain = NVTX.Domain("Armon")
+const range_names = Dict{Symbol, NVTX.StringHandle}()
 
 
 function NVTX_range_start(::ArmonParameters, name::Symbol)
     # More or less equivalent to NVTX.@range
-    NVTX.init!(armon_domain)
-    message = NVTX.StringHandle(armon_domain, string(name))
+    # Note that we do not check for NVTX.isactive(). For some reason Nsight Compute might not enable
+    # NVTX even when passing '--nvtx', while handling NVTX calls correctly.
+    message = get!(range_names, name) do 
+        str_hdl = NVTX.StringHandle(armon_domain, string(name))
+        NVTX.init!(str_hdl)  # Implicitly initializes 'armon_domain' if needed
+        str_hdl
+    end
     color = hash(name) % UInt32
-
     return NVTX.range_start(armon_domain; message, color)
 end
 
